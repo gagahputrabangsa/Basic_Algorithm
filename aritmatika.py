a4672958e731be137c2274aea33e5d022126a212
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-20 00:38:49 +0700

    Update aritmatika.py

--- aritmatika.py
+++ aritmatika.py
@@ -4,8 +4,14 @@ angka_kedua = int(input('Masukkan bil kedua: '))
 #case 1
 jumlah= angka_pertama+angka_kedua
 print(jumlah)
-
-
+#case 2
+#1 angka kedua > angka pertama
+if angka_kedua > angka_pertama:
+    selisih = angka_kedua-angka_pertama
+#2 angka pertama > angka kedua
+else:
+    selisih = angka_pertama - angka_kedua
+print(selisih)
 
 
 
