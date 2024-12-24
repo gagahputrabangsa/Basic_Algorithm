7446aa3d4024ced9eba2ee315a64ce2f8a70d038
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-24 14:08:18 +0700

    Update sum2.py

--- operator/sum2.py
+++ operator/sum2.py
@@ -1,5 +1,6 @@
-first_num = int(input('enter your first num: '))
-second_num = int(input('enter your second num: '))
-
-result = first_num + second_num
-print(f"{first_num} + {second_num} = {result} ") #formatting result 
+while True:
+  first_num = int(input('enter your first num: '))
+  second_num = int(input('enter your second num: '))
+  
+  result = first_num + second_num
+  print(f"{first_num} + {second_num} = {result} ") #formatting result 
