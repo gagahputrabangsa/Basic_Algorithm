be5d67814e91b19796da9214a9abde051416c0c9
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-24 14:18:51 +0700

    Update sum2.py use try except

--- operator/sum2.py
+++ operator/sum2.py
@@ -1,10 +1,13 @@
 while True:
-  first_num = int(input('enter your first num: '))
-  second_num = int(input('enter your second num: '))
+  try:
+    first_num = int(input('enter your first num: '))
+    second_num = int(input('enter your second num: '))
+    
+    result = first_num + second_num
+    print(f"{first_num} + {second_num} = {result} ") #formatting result
   
-  result = first_num + second_num
-  print(f"{first_num} + {second_num} = {result} ") #formatting result
-
-  asking = input ('wanna continue? (Y/N)').lower() #lowercase all the user input
-  if asking == 'n':
-    break    
+    asking = input ('wanna continue? (Y/N)').lower() #lowercase all the user input
+    if asking == 'n':
+      break    
+except ValueError:
+  print('Invalid input. Please enter integers only.')
