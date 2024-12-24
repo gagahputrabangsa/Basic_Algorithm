93016ac8dbe2906af6b67e60b102a337a231781f
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-24 14:06:47 +0700

    Update sum2.py formatting result

--- operator/sum2.py
+++ operator/sum2.py
@@ -2,3 +2,4 @@ first_num = int(input('enter your first num: '))
 second_num = int(input('enter your second num: '))
 
 result = first_num + second_num
+print(f"{first_num} + {second_num} = {result} ") #formatting result 
