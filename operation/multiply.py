139ab7ef3508fcceadf6e870b7a746b7da9908e6
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-26 14:38:02 +0700

    Update multiply.py

--- operation/multiply.py
+++ operation/multiply.py
@@ -3,4 +3,5 @@ user1 = int(input('num 1: '))
 user2 = int(input('num 2: '))
 
 multiply = user1*user2 #this's for multiplying 2 num and store it into multiply
-print(multiply)
+print(multiply) #this's print result only
+print(f'{user1} x {user2} = {result}') #formatted result
