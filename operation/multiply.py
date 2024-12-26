7e650f5c95aa69f162a72fd0a37c24a604c1d2b5
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-26 14:36:54 +0700

    Update multiply.py

--- operation/multiply.py
+++ operation/multiply.py
@@ -2,4 +2,5 @@
 user1 = int(input('num 1: '))
 user2 = int(input('num 2: '))
 
-multiply = user1*user2 #this's for multiplying 2 num and store it into multipy
+multiply = user1*user2 #this's for multiplying 2 num and store it into multiply
+print(multiply)
