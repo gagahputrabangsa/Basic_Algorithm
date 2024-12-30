60436a4f32b6e261674cd872a13347c5a1da0b4c
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-31 00:22:03 +0700

    Update orStatement.py

--- operator/orStatement.py
+++ operator/orStatement.py
@@ -4,5 +4,5 @@ q = input('What gender are you? (Male/Female) ')
 if q == 'Male' or q== 'male': 
   print('You are Male')
 
-elif q == 'Male' or q== 'female':
+elif q == 'Male' or q== 'female': #this Or statement will choose either you input Female or female, it will still running correctly 
   print('You are Female')
