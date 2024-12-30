9e5a29637590ea26a418a923aa77daa163d20ea4
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-12-31 00:21:13 +0700

    Update orStatement.py

--- operator/orStatement.py
+++ operator/orStatement.py
@@ -1,5 +1,8 @@
 #this's simple implementation of an OR operator using if statement
 q = input('What gender are you? (Male/Female) ')
 
-if q == 'Male' or 'male':
+if q == 'Male' or q== 'male': 
   print('You are Male')
+
+elif q == 'Male' or q== 'female':
+  print('You are Female')
