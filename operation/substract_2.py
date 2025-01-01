bd33738a0e369267652e3a3c4c66f4f5d530abb8
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-02 00:11:20 +0700

    Update substract_2.py

--- operation/substract_2.py
+++ operation/substract_2.py
@@ -3,3 +3,4 @@ candy = int(input("How many candy-> "))
 person = int(input("How many person-> "))
 
 candyPerperson = candy/person
+print (f"{candy} Candy can be divide into {candyPerperson}Candy/Person")
