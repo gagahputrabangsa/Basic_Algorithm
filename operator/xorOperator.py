0a420a7114ee559e777482935acd1de853575e01
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-01 23:46:21 +0700

    Update xorOperator.py

--- operator/xorOperator.py
+++ operator/xorOperator.py
@@ -3,4 +3,8 @@
 def xor_integers(a, b):
     """Performs XOR operation on two integers."""
     return a ^ b
+def xor_booleans(a, b):
+    """Performs XOR operation on two boolean values."""
+    return a ^ b
+
 
