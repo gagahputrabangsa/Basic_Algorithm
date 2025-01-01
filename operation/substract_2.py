1f37a76147c35e49b7a8b3e6e5585a159ddc1ef3
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-02 00:13:05 +0700

    Update substract_2.py

--- operation/substract_2.py
+++ operation/substract_2.py
@@ -4,3 +4,5 @@ person = int(input("How many person-> "))
 
 candyPerperson = candy/person
 print (f"{candy} Candy can be divide into {candyPerperson}Candy/Person")
+#if we want to take a absolute amout of candy, we can use int like this
+print (f"{candy} Candy can be divide into {int(candyPerperson)}Candy/Person")
