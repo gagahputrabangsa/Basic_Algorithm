8ae33967b44cd7d77a7e1c8b4ff2a794ff703881
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-03 01:16:18 +0700

    Update modulus.py

--- operation/modulus.py
+++ operation/modulus.py
@@ -1,3 +1,6 @@
 #this program is about simple implementation of modulus operation
 user = int(input("Enter any Num -> "))
 mod = int(input("Enter any modulo-> "))
+
+#modulus is signed as % (percent)
+result = user%mod 
