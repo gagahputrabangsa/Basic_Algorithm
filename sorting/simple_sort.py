a45e5ad944f7e75d19cd6dbaaf79531a1c04e680
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-04 02:51:48 +0700

    Update simple_sort.py

--- sorting/simple_sort.py
+++ sorting/simple_sort.py
@@ -2,12 +2,12 @@
 
 num_loop = int(input('enter num of list: '))
 list = [] # define the list outside the loop
+iterate = 1 #give an iteration
 for i in range (num_loop):
-  iterate = 1 #give an iteration
   num_sort = int(input(f'enter num {iterate} -> '))
   list.append(num_sort) #append the num to list
   iterate +=1 #use incursive to the iterate var
 
 print('List ->',list) #unsorted or Original list
-print('Sorted ->',list) #sorted list from the smallest -> largest
+print('Sorted ->',sorted(list)) #sorted list from the smallest -> largest using sorted func
   
