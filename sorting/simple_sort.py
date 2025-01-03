6e057ff6bf4d9451b130fdfa0f67d1df795ee0f7
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-04 02:45:51 +0700

    Update simple_sort.py

--- sorting/simple_sort.py
+++ sorting/simple_sort.py
@@ -1,11 +1,13 @@
 #this program to implement sort list using input user
 
 num_loop = int(input('enter num of list: '))
-
+list = [] # define the list outside the loop
 for i in range (num_loop):
   iterate = 1
-  list = []
+
   num_sort = int(input(f'enter num {num_loop} -> '))
-  list
+  list.append(num_sort)
   num_sort +=1
+
+print(list)
   
