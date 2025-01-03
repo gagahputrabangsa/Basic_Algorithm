536c853a3fe32ae40db8e823b893ea109ce7177e
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-04 02:46:11 +0700

    Update simple_sort.py

--- sorting/simple_sort.py
+++ sorting/simple_sort.py
@@ -4,10 +4,9 @@ num_loop = int(input('enter num of list: '))
 list = [] # define the list outside the loop
 for i in range (num_loop):
   iterate = 1
-
-  num_sort = int(input(f'enter num {num_loop} -> '))
+  num_sort = int(input(f'enter num {iterate} -> '))
   list.append(num_sort)
-  num_sort +=1
+  iterate +=1
 
 print(list)
   
