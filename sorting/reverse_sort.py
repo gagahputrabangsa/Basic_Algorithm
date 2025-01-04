67a829f78ca34dcbcb89ea49995694488190b6d1
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-05 01:14:54 +0700

    Update reverse_sort.py

--- sorting/reverse_sort.py
+++ sorting/reverse_sort.py
@@ -4,4 +4,7 @@ num_loop = int(input('enter num of list: '))
 list = [] # define the list outside the loop
 iterate = 1 #give an iteration
 for i in range (num_loop):
-  
+  num_sort = int(input(f'enter num {iterate} -> '))
+  list.append(num_sort) #append the num to list
+  iterate +=1 #use incursive to the iterate var
+
