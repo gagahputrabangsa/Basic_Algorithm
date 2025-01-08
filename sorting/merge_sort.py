d0f84920580f7277ebd37ab5db02c16308950383
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-09 00:33:10 +0700

    Update merge_sort.py

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -1,1 +1,10 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
 
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        
