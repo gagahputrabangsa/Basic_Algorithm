721fc98b6abddbc8b7bbb21acb85f8986fa49413
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-11 00:30:25 +0700

    Update heap_sort.py

--- sorting/heap_sort.py
+++ sorting/heap_sort.py
@@ -2,3 +2,12 @@ def heapify(arr, n, i):
     largest = i
     left = 2 * i + 1
     right = 2 * i + 2
+    if left < n and arr[i] < arr[left]:
+        largest = left
+
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
