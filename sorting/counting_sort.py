a28288ed17b5f160cdd58b7ad03e5428184d2255
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-12 22:01:56 +0700

    Update counting_sort.py

--- sorting/counting_sort.py
+++ sorting/counting_sort.py
@@ -3,3 +3,7 @@ def counting_sort(arr):
     count = [0] * (max_val + 1)
     for num in arr:
         count[num] += 1
+
+    sorted_arr = []
+    for i, c in enumerate(count):
+        sorted_arr.extend([i] * c)
