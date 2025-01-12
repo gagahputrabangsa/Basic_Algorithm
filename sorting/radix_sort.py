c8dd5312f94df112d776039add4bcfce47fc1efc
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-13 02:09:35 +0700

    Update radix_sort.py

--- sorting/radix_sort.py
+++ sorting/radix_sort.py
@@ -2,3 +2,7 @@ def counting_sort_for_radix(arr, exp):
     n = len(arr)
     output = [0] * n
     count = [0] * 10
+
+    for i in range(n):
+        index = (arr[i] // exp) % 10
+        count[index] += 1
