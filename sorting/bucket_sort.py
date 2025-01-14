89115910cf31635da14e3e6c290f34d6035ba914
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-14 23:45:15 +0700

    Update bucket_sort.py

--- sorting/bucket_sort.py
+++ sorting/bucket_sort.py
@@ -1,1 +1,8 @@
+def bucket_sort(arr):
+    if len(arr) == 0:
+        return arr
 
+    min_val = min(arr)
+    max_val = max(arr)
+    bucket_count = len(arr)
+    buckets = [[] for _ in range(bucket_count)]
