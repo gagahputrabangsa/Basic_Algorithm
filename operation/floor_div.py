36cb19b0957e8f9c350d4acf984e9b3a32dc5af3
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-16 02:17:03 +0700

    Update floor_div.py

--- operation/floor_div.py
+++ operation/floor_div.py
@@ -7,3 +7,5 @@ division_result = number1 / number2
 print(f"Normal division result: {number1} / {number2} = {division_result}")
 
 # Floor division (integer division)
+floor_division_result = number1 // number2
+print(f"Floor division result: {number1} // {number2} = {floor_division_result}")
