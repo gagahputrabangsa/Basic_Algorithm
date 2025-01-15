7cc8f6ff16b31fd35f84a43de79ec32cfa43c2d1
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-16 02:16:47 +0700

    Update floor_div.py

--- operation/floor_div.py
+++ operation/floor_div.py
@@ -3,3 +3,5 @@ number1 = float(input("Enter the first number: "))
 number2 = float(input("Enter the second number: "))
 
 # Normal division
+division_result = number1 / number2
+print(f"Normal division result: {number1} / {number2} = {division_result}")
