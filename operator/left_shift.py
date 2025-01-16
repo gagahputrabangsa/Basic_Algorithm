e278971f08a053ef3ec83a84bf422806fdc115df
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-17 03:52:55 +0700

    Update left_shift.py

--- operator/left_shift.py
+++ operator/left_shift.py
@@ -1,3 +1,6 @@
 # Input a number from the user
 number = int(input("Enter a number: "))
 shift_amount = int(input("Enter the number of positions to shift left: "))
+
+# Perform the left shift
+shifted_result = number << shift_amount
