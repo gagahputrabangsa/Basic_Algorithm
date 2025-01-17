2e49d9c856fa4c7995c23093d04f4f20d5dde453
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-18 00:51:40 +0700

    Update right_shift.py

--- operator/right_shift.py
+++ operator/right_shift.py
@@ -3,5 +3,8 @@
 # Input a number from the user
 number = int(input("Enter a number: "))
 shift_amount = int(input("Enter the number of positions to shift right: "))
+# Perform the right shift
+shifted_result = number >> shift_amount
+
 
 
