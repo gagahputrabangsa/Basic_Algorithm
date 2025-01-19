20ffb98ed9d36893a16805d9f1a82f3b7fd55044
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-20 00:35:15 +0700

    Update NotIn_operator.py

--- membership operator/NotIn_operator.py
+++ membership operator/NotIn_operator.py
@@ -5,3 +5,4 @@ fruits = ["apple", "banana", "cherry"]
 # Check if a value is not in the list
 fruit_to_check = "orange"
 if fruit_to_check not in fruits:
+    print(f"{fruit_to_check} is not in the list.")
