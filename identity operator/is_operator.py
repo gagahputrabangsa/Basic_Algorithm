3a9fba0b5a2dee252e93d0ee34ed589e583801f4
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-21 16:22:19 +0700

    Update is_operator.py'

--- identity operator/is_operator.py
+++ identity operator/is_operator.py
@@ -7,4 +7,5 @@ c = [1, 2, 3]  # c is a different object with the same content
 # Check if a and b refer to the same object
 print(f"a is b: {a is b}")  # True because b is the same object as a
 
-
+# Check if a and c refer to the same object
+print(f"a is c: {a is c}")  # False because c is a different object
