9776242506f171c54549862a270600cb2f6982ad
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-22 02:19:13 +0700

    Update isNot_operator.py

--- identity operator/isNot_operator.py
+++ identity operator/isNot_operator.py
@@ -10,3 +10,6 @@ c = [1, 2, 3]  # c is a different object with the same content
 
 # Check if a and c do not refer to the same object
 print(f"a is not c: {a is not c}")  # True because c is a different object
+
+# Check if a and b do not refer to the same object
+print(f"a is not b: {a is not b}")  # False because b is the same object as a
