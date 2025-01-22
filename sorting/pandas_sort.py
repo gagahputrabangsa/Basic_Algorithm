6715936666df56f67ca50c6b78b46e0cb6cebdbb
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-23 01:35:48 +0700

    Update pandas_sort.py

--- sorting/pandas_sort.py
+++ sorting/pandas_sort.py
@@ -1,4 +1,4 @@
 import pandas as pd #we're using pandas lib for sorting
 
 data = pd.DataFrame({'Name': ['Alice', 'Bob', 'Charlie'], 'Age': [25, 30, 20]}) #this is example df
-
+sorted_data = data.sort_values('Age')  # sorting df based on their age
