4f5fe52e5a2cd478d6fa592459c9ecf50a0fa8ca
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-24 02:41:36 +0700

    Update increment.py

--- increment.py
+++ increment.py
@@ -1,6 +1,9 @@
+import keyboard
 counter = 1
 while True:
   
   inputs = (input(f'This is counter number-> {counter}'))
   #apply increment method
   counter += 1
+  if keyboard.is_pressed('q'):  # Exit loop on q key
+    break
