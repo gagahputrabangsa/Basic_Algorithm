395854d23d4c2bd1d0ec488ca59f15dc6e6e7614
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-24 02:36:31 +0700

    Update increment.py

--- increment.py
+++ increment.py
@@ -1,6 +1,6 @@
 counter = 1
 while True:
   
-  inputs = (input(f'This is counter number->{counter}'))
+  inputs = (input(f'This is counter number-> {counter}'))
   #apply increment method
   counter += 1
