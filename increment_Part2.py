dbb302eeaf13343f9bb4d1a16708b346f197e186
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-01-25 17:20:40 +0700

    Create increment_Part2.py

---
+++ increment_Part2.py
@@ -0,0 +1,10 @@
+# Extended Increment Program
+
+def increment_number(number):
+    """Function to increment a number by 1."""
+    return number + 1
+
+def reset_number():
+    """Function to reset the number to 0."""
+    return 0
+
