f007c6cf4b46d97e6d3b33c409f35a83b6459a49
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-02-02 02:43:26 +0700

    Update wloop1.py

--- looping/for loop/while_loop/wloop1.py
+++ looping/for loop/while_loop/wloop1.py
@@ -1,3 +1,3 @@
 while True: #it means that while the condition is true, it will continuously looping
-  print('Hello World')
-  break;
+  print('Hello World') #this will printout HelloWorld over and over until condition is either false or i break it  
+  break; #i break it here, so it will loop once
