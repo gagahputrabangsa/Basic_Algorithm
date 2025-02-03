d4a52ab9f9b6dcd558ee5b4d43ce17c381f0abca
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-02-04 00:15:31 +0700

    Update wloop3.py

--- looping/while loop/wloop3.py
+++ looping/while loop/wloop3.py
@@ -5,5 +5,7 @@ print("Welcome to the Number Guessing Game!")
 lower_bound = int(input("Enter the lower bound for the number range: "))
 upper_bound = int(input("Enter the upper bound for the number range: "))
 
-
+# Generate a secret number to guess within the given range
+import random
+secret_number = random.randint(lower_bound, upper_bound)
 
