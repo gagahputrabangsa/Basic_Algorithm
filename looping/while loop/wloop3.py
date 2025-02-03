23a9419a96113a9e8722fd36ce5fd2a292380682
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-02-04 00:15:14 +0700

    Update wloop3.py

--- looping/while loop/wloop3.py
+++ looping/while loop/wloop3.py
@@ -1,1 +1,9 @@
+# Introduction message
+print("Welcome to the Number Guessing Game!")
+
+# Ask the user for the range of numbers to guess
+lower_bound = int(input("Enter the lower bound for the number range: "))
+upper_bound = int(input("Enter the upper bound for the number range: "))
+
+
 
