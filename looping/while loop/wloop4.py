13089a4ef37405bee0ff40dc9c2225500a36127a
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-02-07 03:31:37 +0700

    Update wloop4.py

--- looping/while loop/wloop4.py
+++ looping/while loop/wloop4.py
@@ -1,1 +1,12 @@
+import time
+
+def countdown(n):
+  """
+  This function performs a countdown from n to 1, pausing for 1 second at each number.
+  """
+  while n > 0:
+    print(n)
+    n -= 1
+    time.sleep(1)
+
 
