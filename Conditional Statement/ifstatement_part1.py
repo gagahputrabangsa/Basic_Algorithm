3b509101a5070e16440d961df3bbde308fece793
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-02-08 03:12:21 +0700

    Update ifstatement_part1.py

--- Conditional Statement/ifstatement_part1.py
+++ Conditional Statement/ifstatement_part1.py
@@ -1,3 +1,6 @@
 #if statement is like when some condition is met, then the rest of the code inside it will be executed
 
 name = input("Please Enter Your Name: ") #var
+#place the conditional statement here
+if name == 'gaga': 
+  print("Welcome Master ><") #so, if i entered 'gaga' as my name, it will printout Welcome Gaga ><
