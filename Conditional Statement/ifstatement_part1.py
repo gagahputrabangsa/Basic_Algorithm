c7cb7b53b34317f4b1df4bd434c8e2ceb76f8da4
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-02-08 03:13:24 +0700

    Update ifstatement_part1.py

--- Conditional Statement/ifstatement_part1.py
+++ Conditional Statement/ifstatement_part1.py
@@ -3,4 +3,6 @@
 name = input("Please Enter Your Name: ") #var
 #place the conditional statement here
 if name == 'gaga': 
-  print("Welcome Master ><") #so, if i entered 'gaga' as my name, it will printout Welcome Gaga ><
+  print("Welcome Master ><") #so, if I entered 'gaga' as my name, it will printout Welcome Gaga ><
+#now if the name is other than gaga:
+print("Who Are You?")
