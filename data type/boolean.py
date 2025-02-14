f0a11dfe2f8b6a853e52b2e55b7a4dcd0c5a2451
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-02-15 00:04:06 +0700

    Update boolean.py

--- data type/boolean.py
+++ data type/boolean.py
@@ -4,4 +4,12 @@
 is_raining = True
 is_sunny = False
 
+print("Boolean Data Type Example")
+print("Is it raining?", is_raining)
+print("Is it sunny?", is_sunny)
 
+# Boolean expressions
+print("5 > 3:", 5 > 3)  # True
+print("10 == 20:", 10 == 20)  # False
+
+# Using Boolean in conditions
