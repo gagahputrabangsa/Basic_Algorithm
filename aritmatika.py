2a998dd166b19217e82850e97cafe13826bce725
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2025-02-19 02:13:24 +0700

    Update aritmatika.py

--- aritmatika.py
+++ aritmatika.py
@@ -2,6 +2,13 @@ first_num = int(input('Masukkan bil pertama: '))
 second_num = int(input('Masukkan bil kedua: '))
 
 #case 1
+jumlah= first_num+second_num
+print(jumlah)
+#case 2
+#1 second num > first num
+if second_num > first_num:
+    selisih = second_num-first_num
+#2 first num > second num
 
 
 
